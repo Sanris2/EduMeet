ace030ed5565cf2c071aa168f8f29ae6e8ed471c
sanjana prasad <prasad9sanjana@gmail.com>
2025-04-26 22:41:01 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,17 @@ const dotenv = require('dotenv')
 const app = require('./app')
 
 dotenv.config({path: './config.env'})
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log('Database connection successful!');
+    })
+    .catch((err) => {
+        console.log('Error 💥', err.message)
+    })
+
 
 const port = process.env.PORT;
 app.listen(port, () => {
